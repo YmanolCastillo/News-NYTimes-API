7d54c0be89bb149cbe9f769fa59e4d48fe799bf5
Ymanol Martinez <ymanolcastillo@gmail.com>
2021-05-23 03:34:16 -0400

    Fixing some and changing frontend view

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from 'src/app/_services/news.service';
-import { map } from 'rxjs/operators';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,26 +8,35 @@ import { Subscription } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
   data: any;
-  media: any;
   id: number;
-  article: any;
-  constructor(
-    http: HttpClient,
-    private newsService: NewsService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {}
+  newsAuthor: string;
+  dayNews: boolean;
+  weekNews: boolean;
+  constructor(private _newsService: NewsService, private router: Router) {}
 
-  ngOnInit(): void {
-    this.newsService.getNews('shared/7.json').subscribe((data) => {
+  ngOnInit() {
+    this.dayNews = true;
+    //Request for 1 Day News
+    this._newsService.getNews('shared/1.json').subscribe((data) => {
       this.data = data;
       console.log(data);
-
-      // console.log(data['results'][1].media[0]['media-metadata'][0].url);
+    });
+    //Request for 7 Days (Week) News
+    this._newsService.getNews('shared/7.json').subscribe((data) => {
+      this.data = data;
     });
   }
 
   onNewsDetails(id) {
     this.router.navigateByUrl(`/news-details/${id}`);
+  }
+
+  showDayNews() {
+    this.weekNews = false;
+    this.dayNews = true;
+  }
+  showWeekNews() {
+    this.dayNews = false;
+    this.weekNews = true;
   }
 }
--- src/app/pages/news-details/news-details.component.ts
+++ src/app/pages/news-details/news-details.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { News } from 'src/app/_models/news.interface';
 import { NewsService } from 'src/app/_services/news.service';
 
 @Component({
@@ -11,10 +9,7 @@ import { NewsService } from 'src/app/_services/news.service';
 })
 export class NewsDetailsComponent implements OnInit {
   id: any;
-  paramsSubscription: Subscription;
   data: any;
-  article: any;
-  dataInfo: News = new News();
   constructor(
     private _newsService: NewsService,
     private _route: ActivatedRoute,
@@ -22,15 +17,14 @@ export class NewsDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    //Adding ID to the Route Path
     this._route.paramMap.subscribe((params: ParamMap) => {
       this.id = params.get('id');
     });
 
+    //Getting New from the Route Path ID
     this._newsService.getNews('shared/7.json').subscribe((data) => {
       this.data = data['results'][this.id];
-      console.log(this.data['media'][0]['media-metadata'][0]['url']);
-
-      this.dataInfo.getObject(this.data);
     });
   }
 
